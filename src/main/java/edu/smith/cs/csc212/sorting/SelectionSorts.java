55c6d6d7f595c852782dfa2ddae7fff4133fc12f
Owner <Owner@131.229.148.60>
2019-11-07 11:00:44 -0500

    Selection Sort done.

--- src/main/java/edu/smith/cs/csc212/sorting/SelectionSorts.java
+++ src/main/java/edu/smith/cs/csc212/sorting/SelectionSorts.java
@@ -1,5 +1,23 @@
 package edu.smith.cs.csc212.sorting;
 
-public class SelectionSorts {
+import me.jjfoley.adt.ListADT;
 
+public class SelectionSorts {
+	public static void sort(ListADT<Integer> input) {
+		int S = input.size();
+		for (int i = 0; i < S - 1; i++) {
+			int small = i;
+			for (int big = i + 1; big < small; big++) {
+				if (input.getIndex(big) < input.getIndex(small)) {
+					small = big;
+					
+					int swap = small;
+					small = i;
+					i = swap;
+				}
+
+			}
+		}
+
+	}
 }
